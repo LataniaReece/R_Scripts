bcd620b1891987fb24b8ef375e444d053a9c87d4
RTania118 <48932236+RTania118@users.noreply.github.com>
2019-04-19 12:39:40 -0400

    Update Checking_missing.R

--- Base_Scripts/Checking_missing.R
+++ Base_Scripts/Checking_missing.R
@@ -2,5 +2,5 @@ percent_miss <- function(x) {(sum(is.na(x))/length(x))*100}
 rowmiss <- apply(rawdata, 1, percent_miss)
 colmiss <- apply(rawdata, 2, percent_miss)
 
-crit_part_miss <- subset(rawdata, rowmiss > 50)
+crit_obs_miss <- subset(rawdata, rowmiss > 50)
 crit_var_miss <- rawdata[colmiss > 50]
